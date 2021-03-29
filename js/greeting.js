c2a048b9e81efdb62600175aa6063f4f8060db0c
Zoe Kendall <zskendall@gmail.com>
2021-03-29 13:12:33 -0400

    add periodic refresh and name to greeting

--- js/greeting.js
+++ js/greeting.js
@@ -1,25 +1,29 @@
-// Get the hour
-const today = new Date();
-const hour = today.getHours();
-
 // Here you can change your name
-const name = ' John Doe';
+const name = 'Zoe';
+
+window.onload = displayGreeting();
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep!  ';
-const gree2 = 'Good morning!  ';
-const gree3 = 'Good afternoon  ';
-const gree4 = 'Good evening,  ';
-const gree5 = 'Good evening,  ';
-const gree6 = 'Good evening,  ';
+function greet(hour) {
+  if (hour < 5) {
+    const qual = hour < 3 ? ' ' : ' the fuck ';
+    return `Go${qual}to sleep!`;
+  } else if (hour >= 6 && hour < 12) {
+    return `Good morning, ${name}!`;
+  } else if (hour >= 12 && hour < 18) {
+    return `Good afternoon, ${name}`;
+  } else {
+    return `Good evening, ${name}`;
+  }
+}
 
-// Define the hours of the greetings
-if (hour >= 23 && hour < 5) {
-  document.getElementById('greetings').innerText = gree1 + name;
-} else if (hour >= 6 && hour < 12) {
-  document.getElementById('greetings').innerText = gree2 + name;
-} else if (hour >= 12 && hour < 17) {
-  document.getElementById('greetings').innerText = gree3 + name;
-} else {
-  document.getElementById('greetings').innerText = gree4 + name;
+function displayGreeting() {
+  // Get the hour
+  const today = new Date();
+  const hour = today.getHours();
+  
+  // Define the hours of the greetings
+  document.getElementById('greetings').innerText = greet(hour);
+  
+  setTimeout(displayGreeting, 1000 * 60 * 15); // refresh every 15 min
 }
